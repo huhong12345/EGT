99375313f4f3c5a1c619b26420646e132a0aefd8
huhong12345 <hhuxidian@gmail.com>
2019-04-22 12:41:06 +0800

    huhong

--- Code_Backup/aa.m
+++ Code_Backup/aa.m
@@ -1,59 +1,86 @@
 function result = FourStratery_simulate_im_over_regular_graph(U,graph, alpha, iterate_time, N,k)
     function fit_result = fitness_calculate(index)
         fit = (1-alpha);     %fit=(1-alpha)+alpha*U
-        l=1;
-        for l<=k;
-            fit=fit+alpha*U(index,strategy_state(graph(index,l)));
-            l=l+1;  
+        for l=1:k
+            fit=fit+alpha*U(index,strategy_state(graph(index,l))); 
         end
         fit_result = fit;
     end
 
-
-
-
     strategy_state = zeros(1,N);  %定义出长度为N的0矩阵（1*N)
     z=[0.05,0.025,0.025,0.9];           %define the percentage of every strategy
+    K=N*z;
     starter_table=randperm(N);          %打乱的
-    S1=[]
+    S1=[];S2=[];S3=[];S4=[];
+    S1=zeros(1,N);
+    S2=zeros(1,N);
+    S3=zeros(1,N);
+    S4=zeros(1,N);
+%    S=[S1;S2;S3;S4];
+
+    
     for i=1:N                           %define the strategy state 1,2,3,4
-        if i<=N*z[1]
+        if i<=K(1)
             strategy_state(starter_table(i))=1;
-            S1=[]
+            S1=[S1,starter_table(i)]
+   %         S1(i)=strategy_state(i);
         end
-        if N*z[1]<i<=N*z[2]
+        if K(1)<i<=K(2)
             strategy_state(starter_table(i))=2;
+            S2=[S2,starter_table(i)]
+%            S2(i-K(1)=strategy_state(i);
         end
-        if N*z[2]<i<=N*z[3]
+        if K(2)<i<=K(3)
             strategy_state(starter_table(i))=3;
+            S3=[S3,starter_table(i)]
+%            S3(i-K(2))=strategy_state(i);
         end
-        if i>N*z[3]
+        if i>K(3)
             strategy_state(starter_table(i))=4;
+            S4=[S4,starter_table(i)]
+%           S4(i-K(3))=strategy_state(i);
         end   
-    end     
+    end  
+    S=[S1;S2;S3;S4];   
+
 
     x=zeros(4,iterate_time);
     time = 1;
     for i=1:4
-        x(i,time) = sum(strategy_state)/(i*N);     %initialize the start 
+        x(i,time) = K(i)/N;     %initialize the start 
     end
-
     time = time + 1;
     while time<=iterate_time
         %每次遍历全部点，并根据IM——rule更新
          for p = 1:N
-            i = randi(N);    %%随机从1000个人中选一个i
-            friend_list=friend_matrix(i,:);
-
-
-
-
-
-
-            friend_list = find_friend(i,N);    %寻找i的邻居，返回一个1*N的矩阵，前k个有数，后面为0
-            friend_number = length(friend_list);   %k
-            %calculate self-fitness
+            i = randi(N);    %%随机从N个人中选一个i
+            friend_list=graph(i,:);
             fit_self = fitness_calculate(i);    %算出自己的fitness
+            fit1=0;fit2=0;fit3=0;fit4=0;
+v
+
+            if strategy_state(i)==1
+                for j=1:k
+                    fit
+
+
+                    if strategy_state(graph(i,j))
+                        
+
+
+
+
+
+            if video
+
+
+
+
+
+%           friend_list = find_friend(i,N);    %寻找i的邻居，返回一个1*N的矩阵，前k个有数，后面为0
+%           friend_number = length(friend_list);   %k
+            %calculate self-fitness
+
             fit_f = 0; 
             fit_n = 0;
             if strategy_state(i)==1          %如果i自己forward
