66e7cfd1472a6a388ad3f052bd43a228cb6f3789
huhong12345 <hhuxidian@gmail.com>
2019-05-05 13:32:39 +0800

    胡泓、

--- Code_Backup/Main.m
+++ Code_Backup/Main.m
@@ -4,8 +4,8 @@ N=1000; %The scale of the Graph
 k=20; %The degree of a node
 alpha=0.1;      %weak-connection parameter
 iteration_time=400;  
-G_N=10;       %The Repeating Graphs of simulation
-S_M=24;       %The Repeating times of simulation on one graph
+G_N=2;       %The Repeating Graphs of simulation
+S_M=8;       %The Repeating times of simulation on one graph
 Ua=[0.6,0.8;0.8,0.4];
 Ub=[0.6,0.8;0.8,0.4];
 %U=[];         %4*4 payoff matrix!!!!!!!!!!!!!!!!!!!!!!
@@ -29,8 +29,7 @@ for i = 1:G_N
     end
     Graphth_Result(i,:,:) = mean(Iteration_Results,1);          %第i个图，跑了S_M遍，
     toc
-    %Graphth_Result=gather(
-        Graphth_Result);
+    %Graphth_Result=gather(Graphth_Result);
 end
 
     Final_Results = mean(Graphth_Result,1);
